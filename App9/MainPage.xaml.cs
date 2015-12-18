7843dafc2d6c1ff686f3b66e8cfde77d213bc3cf
Mikael Koskinen <mijapkos@gmail.com>
2015-12-18 10:10:53 +0200

    Example for http://stackoverflow.com/questions/34308815/winrt-dependencyproperty-is-never-set

---
+++ App9/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App9
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var peopleList = new List<PeopleModel>
+            {
+                new PeopleModel("Hello"),
+                new PeopleModel("Hi"),
+                new PeopleModel("Hey")
+            };
+
+            var peopleList2 = new List<PeopleModel>
+            {
+                new PeopleModel("More"),
+                new PeopleModel("Names"),
+            };
+
+            var list = new PeopleList(peopleList);
+            var list2 = new PeopleList(peopleList2);
+
+            this.DataContext = new MainPageVm(new List<PeopleList> { list, list2});
+        }
+    }
+
+    public class MainPageVm
+    {
+        public List<PeopleList> LinePeopleList { get; set; }
+
+        public MainPageVm(List<PeopleList> people)
+        {
+            LinePeopleList = people;
+        }
+    }
+
+    public class PeopleList
+    {
+        public List<PeopleModel> People { get; set; }
+
+        public PeopleList(List<PeopleModel> linePeopleList)
+        {
+            People = linePeopleList;
+        }
+    }
+}
---
+++ App9/PeopleLine.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace App9
+{
+    public sealed partial class PeopleLine : UserControl
+    {
+        public static readonly DependencyProperty PeopleListProperty = DependencyProperty.Register("PeopleList", typeof(List<PeopleModel>), typeof(PeopleLine), new PropertyMetadata(default(List<PeopleModel>)));
+
+        public static readonly DependencyProperty PeopleModelsProperty = DependencyProperty.Register(
+            "PeopleModels", typeof (List<PeopleModel>), typeof (PeopleLine), new PropertyMetadata(default(List<PeopleModel>)));
+
+        public List<PeopleModel> PeopleList
+        {
+            get { return (List<PeopleModel>)GetValue(PeopleListProperty); }
+            set { SetValue(PeopleListProperty, value); }
+        }
+
+        public PeopleLine()
+        {
+            this.InitializeComponent();
+
+            //(this.Content as FrameworkElement).DataContext = this;
+        }
+    }
+
+    public class PeopleModel
+    {
+        public string Name { get; set; }
+
+        public PeopleModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
